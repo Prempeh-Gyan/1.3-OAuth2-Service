5fb86100fb9bacfe4859738738355fd58d4e53df
Prempeh-Gyan <gyan.prempeh@aiesec.net>
2018-02-06 08:42:49 +0100

    default, no test cases yet

--- src/test/java/com/premps/oauth2service/config/ApplicationTests.java
+++ src/test/java/com/premps/oauth2service/config/ApplicationTests.java
@@ -1,5 +1,8 @@
 package com.premps.oauth2service.config;
 
+
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +14,7 @@ public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
